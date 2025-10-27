7bcfbd37dc2c1461a5217a26a8e035033d82530e
SauravKumar-max <maxsaurav237@gmail.com>
2025-10-27 11:39:13 +0530

    - deploying database in neon

--- prisma/migrations/20251027060607_init/migration.sql
+++ prisma/migrations/20251027060607_init/migration.sql
@@ -1,10 +1,12 @@
 -- CreateTable
 CREATE TABLE "url_shortener" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "original_url" TEXT NOT NULL,
     "short_code" TEXT NOT NULL,
     "visit_count" INTEGER NOT NULL DEFAULT 0,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "url_shortener_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
