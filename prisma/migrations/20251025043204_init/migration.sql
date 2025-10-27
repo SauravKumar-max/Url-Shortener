8735b8a868dfc641f438e739fc3a175db17f7582
SauravKumar-max <maxsaurav237@gmail.com>
2025-10-27 11:07:13 +0530

    Revert database deployment

--- prisma/migrations/20251025043204_init/migration.sql
+++ prisma/migrations/20251025043204_init/migration.sql
@@ -1,12 +1,10 @@
 -- CreateTable
 CREATE TABLE "url_shortener" (
-    "id" SERIAL NOT NULL,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "original_url" TEXT NOT NULL,
     "short_code" TEXT NOT NULL,
     "visit_count" INTEGER NOT NULL DEFAULT 0,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "url_shortener_pkey" PRIMARY KEY ("id")
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- CreateIndex
